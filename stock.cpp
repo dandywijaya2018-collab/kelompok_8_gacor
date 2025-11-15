c697a97201005f7bd8ef8207720258bc98e085c6
REZA JUANSYAHPUTRA <reza juansyahputra@reza.com>
2025-11-15 21:31:50 +0700

    nambah barang

---
+++ stock.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void barangAwal() {
+    dataBarang[0] = {"Rokok Marlboro", 20, "Rak 4"};
+    dataBarang[1] = {"Rokok Sampoerna", 20, "Rak 4"};
+    dataBarang[2] = {"Kopi", 10, "Rak 1"};
+    dataBarang[3] = {"Gula", 50, "Rak 2"};
+    dataBarang[4] = {"Teh", 60, "Rak 3"};
+    
+    jumlahBarang = 5; 
+
+}
+
